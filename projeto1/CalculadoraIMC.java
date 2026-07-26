f3293e2953d0d2a6c8edce75ed11ad256474de39
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-07-26 15:40:52 +0000

    feat: add field and weight labels

---
+++ projeto1/CalculadoraIMC.java
@@ -0,0 +1,15 @@
+import javafx.application.*;
+import javafx.geometry.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+
+public class CalculadoraIMC extends Application {
+    @Override 
+    public void start(Stage palco) {
+        // labels for input fields
+        Label weightField = new Label("Weight");
+        Label heightField = new Label("Height");
+    }
+}
