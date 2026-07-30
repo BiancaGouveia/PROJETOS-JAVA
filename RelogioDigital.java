bd5a13e16b3c42f857f4bc507c89cc98f00d3461
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-07-30 01:28:19 +0000

    feat: add all necessary classes

---
+++ RelogioDigital.java
@@ -0,0 +1,12 @@
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.time.LocalDateTime;
+import javafx.time.format.DateTimeFormatter;
