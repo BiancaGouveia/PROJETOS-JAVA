d7c4d7c304b94b2f1b326164dda73f4354e58fe4
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-08-02 01:30:29 +0000

    feat: implementa algoritmo de geração de senhas usando SecureRandom e StringBuilder

--- GeradorSenhasSeguras.java
+++ GeradorSenhasSeguras.java
@@ -3,5 +3,15 @@ import java.security.SecureRandom;
 public class GeradorSenhasSeguras {
     private static final String CARACTERES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%¨&*()_+-*/";
 
- 
+    public static String gerarSenha(int comprimento) {
+        SecureRandom geradorDeNumeroAleatorio = new SecureRandom();
+        StringBuilder senha = new StringBuilder(comprimento);
+
+        for(int i=0; i<comprimento; i++) {
+            int indice = geradorDeNumeroAleatorio.nextInt(CARACTERES.lenght());
+            senha.append(CARACTERES.chatAt(indice));
+        }
+
+        return senha.toString();
+    }
 }
