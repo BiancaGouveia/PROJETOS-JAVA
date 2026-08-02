7afb232e83f468b9276867a10ca27eb8e4634f1b
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-08-02 01:22:14 +0000

    feat: adiciona conjunto de caracteres para geração de senhas

--- GeradorSenhasSeguras.java
+++ GeradorSenhasSeguras.java
@@ -1,2 +1,7 @@
 import java.security.SecureRandom;
 
+public class GeradorSenhasSeguras {
+    private static final String CARACTERES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%¨&*()_+-*/";
+
+ 
+}
