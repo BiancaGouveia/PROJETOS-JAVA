b402adfc0f0dc6e8678d01b95d174821c73d4f1b
Bianca Gouveia <160188183+BiancaGouveia@users.noreply.github.com>
2026-08-05 14:29:42 +0000

    chore: faz o import de todas as classes necessárias

---
+++ projeto_GeradorSenhasSeguras/GeradorSenhasSegurasGUI.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
